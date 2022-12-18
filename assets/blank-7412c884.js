a4dda567ec6bb54674a54103b6f078a72cfef4db
Tran Sam <transamdhkh@gmail.com>
2022-12-18 20:01:16 +0700

    Updates

---
+++ assets/blank-7412c884.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as n,r}from"./index-0fe7b176.js";const a={name:"BlankLayout"};function c(s,_,p,u,f,i){const o=r("RouterView");return t(),n(o)}const m=e(a,[["render",c]]);export{m as default};
